dcdd6fe32a60a59ae9c4bda12001e57f977c146f
federicosaenz@gmail.com <federicosaenz@gmail.com@2206cc79-7971-bcdd-4310-2487ee96ccec>
2011-04-06 17:43:23 +0000

    documentacion de la clase collection

--- trunk/bin/system/Collection.class.php
+++ trunk/bin/system/Collection.class.php
@@ -1,17 +1,43 @@
 <?php
+/**
+ * Contenedor de coleccion de tipos con cache a disco
+ * @author Federico Saenz
+ * @date 06/04/2011
+ *
+ */
 
 final class Collection {
 
-	private $filename;
+	/**
+	 * Ruta del archivo de cache
+	 * @var string
+	 */
+	private $cachefile;
 
-	private $items;
-	
+	/**
+	 * Coleccion
+	 * @var array
+	 */
+	private $items = array();
+
+	/**
+	 * Flag que setea el estado de la cache. Si esta seteada en true se vuelve a generar
+	 * @var <type>
+	 */
 	private $recompile = false;
-	
-	public function __construct($filename) {
-		self::$filename = $filename;
+
+	/**
+	 * Constructor de la clase
+	 * @param string $cacheFile
+	 */
+	public function __construct($cacheFile=null) {
+		self::$cachefile = $cacheFile;
 	}
 
+	/**
+	 * Devuelve la coleccion
+	 * @return array
+	 */
 	public function getCollection() {
 		if(is_readable(self::$filename)) {
 			if(!$this->items) {
@@ -21,8 +47,22 @@ final class Collection {
 		return $this->items;
 	}
 
+	/**
+	 * Devuelve si el item buscado se encuentra en la coleccion
+	 * @param mixed $item (el key de la coleccion)
+	 * @return boolean
+	 */
 	public function hasItem($item) {
 		return (isset($this->items[$item]));
+	}
+
+	/**
+	 * Agrega un item a la coleccion
+	 * @param mixed $item
+	 * @param mixed $value
+	 */
+	public function addItem($item,$value) {
+
 	}
 }
 ?>
