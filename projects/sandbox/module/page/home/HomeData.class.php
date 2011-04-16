f91eb46b087a7d0780f7d7766d38fea79223c47a
federicosaenz <federicosaenz@gmail.com>
2011-04-16 14:46:24 +0000

    cambio en estructura de carpetas y se borran cosas que no van

---
+++ projects/sandbox/module/page/home/HomeData.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Clase de modelo de la home
+ *
+ * @author Federico Saenz
+ * @version 1.0 12/04/2011
+ * @package project
+ * @subpackage page
+ * 
+ */
+
+final class HomeData  {
+
+	
+}
+?>
