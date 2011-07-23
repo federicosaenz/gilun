02e65981e2042b16ad91e308a610c1427bee36fe
federicosaenz <federicosaenz@gmail.com>
2011-07-23 00:10:55 +0000

    conexiones a la base de datos, fase 0

---
+++ bin/connection/IConnection.interface.php
@@ -0,0 +1,26 @@
+<?
+
+/**
+ * Interface para manejo de base de datos
+ *
+ * @package	System
+ * @author	Federico Saenz
+ * @version	1.0 05/01/2009
+*/
+interface IConnection {
+
+//	public function setConnection($name,$read,$write);
+//	public function changeConnection($name);
+//	public function connect();
+
+//	public function hasResource();
+//	public function getResource();
+//	public function insert($query);
+//	public function update($query);
+//	public function delete($query);
+//	public function select($query);
+//	public function get($query);
+//	public function cleanResult();
+//	public function fetch($resourceId);
+}
+?>
---
+++ lib/mysql/Mysql.class.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Motor de manejo de output del tipo html por medio de la Dom
+ *
+ * @author Federico Saenz
+ * @version 1.0 11/04/2011
+ * @package output
+ * @subpackage dom
+ *
+ */
+final class Mysql extends mysqli implements IConnection {
+
+	public function __construct($host, $user, $pass, $db) {
+        parent::__construct($host, $user, $pass, $db);
+
+        if (mysqli_connect_error()) {
+            echo "no se pudo conectar a la base de datos";
+			#Excepcion de que no se pudo conectar a la base de datos
+        }
+	}
+}
+?>
