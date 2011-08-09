8a943c2f673405c445a731064d63a29ddb9d7be9
federicosaenz@gmail.com <federicosaenz@gmail.com@2206cc79-7971-bcdd-4310-2487ee96ccec>
2011-08-09 22:46:55 +0000

    terminando el wizard

--- trunk/panel/modules/install_step1.php
+++ trunk/panel/modules/install_step1.php
@@ -13,7 +13,9 @@ $ButtonClose = new windowButton();
 $ButtonClose->setText("Cerrar");
 $ButtonClose->setId("idButtonClose");
 
-$proyectName = "proyecto1";
+$proyectName	= isset($_GET["name"])			&& $_GET["name"]					? $_GET["name"]				: "proyecto1";
+$configureQa	= isset($_GET["configureQa"])	&& $_GET["configureQa"]=="true";
+$configureProd	= isset($_GET["configureProd"])	&& $_GET["configureProd"]=="true";
 
 ?>
 <div style="padding:20px;">
@@ -22,10 +24,15 @@ $proyectName = "proyecto1";
 			<img src="panel/images/iw1.jpg" style="width:250px;height:400px;animation:suavizado 0.4s 1;-moz-animation:suavizado 0.4s 1;-webkit-animation:suavizado 0.4s 1;" alt="" />
 		</div>
 		<div style="float:left;padding-left:50px;width:380px;">
-			<!-- title --><div style="font-size:22px">Instalando un nuevo proyecto</div>
-			<!-- text --><div style="font-size:13px;padding-top:15px;padding-bottom:50px;">Bienvenido al wizard de instalaci&oacute;n de proyectos <b>Gil&uacute;n</b>. Este wizard lo guiar&aacute; durante la instalaci&oacute;n y configuraci&oacute;n de un nuevo proyecto en la carpeta projects.<br/><br/>Para comenzar, introduzca un nombre para su proyecto (sin puntos, espacios, ni barras) y haga click en el bot&oacute;n Siguiente</div>
-			<!-- label --><span style="font-size:12px;">Nombre del proyecto: </span>
-			<!-- txt --><input type="text" name="proyectName" id="idProyectName" style="border:1px solid #444;color:#444;font-style:italic;" value="<? echo $proyectName?>" />
+			<div style="font-size:22px">Instalando un nuevo proyecto</div>
+			<div style="font-size:13px;padding-top:15px;padding-bottom:10px;">Bienvenido al wizard de instalaci&oacute;n de proyectos <b>Gil&uacute;n</b>. Este wizard lo guiar&aacute; durante la instalaci&oacute;n y configuraci&oacute;n de un nuevo proyecto en la carpeta projects.<br/><br/>Introduzca un nombre para su proyecto (sin puntos, espacios, ni barras) </div>
+			<span style="font-size:12px;">Nombre del proyecto: </span>
+			<input type="text" name="proyectName" id="idProyectName" style="border:1px solid #444;color:#444;font-style:italic;" value="<? echo $proyectName?>" />
+			<hr />
+			<div style="font-size:13px;padding-top:10px;">Adem&aacute;s de dev, que otros entornos desea configurar?<br/><br/>
+				<input type="checkbox" id="idConfigureQa" name="configureQa"	<? echo  $configureQa		? "checked=\"checked\"" :""?> />Test<br/>
+				<input type="checkbox" id="idConfigureProd" name="configureProd" <? echo  $configureProd	? "checked=\"checked\"" :""?> />Producci&oacute;n<br/>
+			</div>
 		</div>
 		<!-- dvClearBoth --><div style="clear:both"></div>
 	</div>
--- trunk/panel/modules/install_step2.php
+++ trunk/panel/modules/install_step2.php
@@ -13,18 +13,49 @@ $ButtonClose = new windowButton();
 $ButtonClose->setText("Cerrar");
 $ButtonClose->setId("idButtonClose");
 
-$domainName = "dev.proyecto1.com";
-$domainUser = "";
-$domainPass = "";
-$env = "dev";
+$env			= isset($_GET["env"])			? $_GET["env"]			: "dev";
+$proyectName	= isset($_GET["name"])			? $_GET["name"]			: "";
+
+$domainName		= isset($_GET["domainName"])	? $_GET["domainName"]	: $env.".".$proyectName.".com";
+$domainUser		= isset($_GET["domainUser"])	? $_GET["domainUser"]	: "";
+$domainPass		= isset($_GET["domainPass"])	? $_GET["domainPass"]	: "";
+
+$dbReadName		= isset($_GET["dbReadName"])	? $_GET["dbReadName"]	: $proyectName;
+$dbReadHost		= isset($_GET["dbReadHost"])	? $_GET["dbReadHost"]	: "localhost";
+$dbReadUser		= isset($_GET["dbReadUser"])	? $_GET["dbReadUser"]	: "root";
+$dbReadPass		= isset($_GET["dbReadPass"])	? $_GET["dbReadPass"]	: "";
+$dbReadPort		= isset($_GET["dbReadPort"])	? $_GET["dbReadPort"]	: "3306";
+
+$dbWriteName	= isset($_GET["dbWriteName"])	? $_GET["dbWriteName"]	: $proyectName;
+$dbWriteHost	= isset($_GET["dbWriteHost"])	? $_GET["dbWriteHost"]	: "localhost";
+$dbWriteUser	= isset($_GET["dbWriteUser"])	? $_GET["dbWriteUser"]	: "root";
+$dbWritePass	= isset($_GET["dbWritePass"])	? $_GET["dbWritePass"]	: "";
+$dbWritePort	= isset($_GET["dbWritePort"])	? $_GET["dbWritePort"]	: "3306";
+
+
+switch($env) {
+	case "dev":
+		$imagesrc			= "panel/images/iw2.jpg";
+		$nameEnvironment	= "desarrollo";
+	break;
+	case "qa":
+		$imagesrc			= "panel/images/iw3.jpg";
+		$nameEnvironment	= "test";
+	break;
+	case "prod":
+		$imagesrc			= "panel/images/iw3.jpg";
+		$nameEnvironment	= "producci&oacute;n";
+	break;
+}
+
 ?>
 <div style="padding:20px;">
 	<div>
 		<div style="float:left">
-			<img src="panel/images/iw1.jpg" style="width:250px;height:400px;animation:suavizado 0.4s 1;-moz-animation:suavizado 0.4s 1;-webkit-animation:suavizado 0.4s 1;" alt="" />
+			<img src="<? echo $imagesrc?>" style="width:250px;height:400px;animation:suavizado 0.4s 1;-moz-animation:suavizado 0.4s 1;-webkit-animation:suavizado 0.4s 1;" alt="" />
 		</div>
 		<div style="float:left;padding-left:50px;width:380px;">
-			<div style="font-size:22px">Configuraci&oacute;n del entorno de desarrollo</div>
+			<div style="font-size:22px">Configuraci&oacute;n del entorno de <?echo $nameEnvironment?></div>
 			<div style="font-size:13px;padding-top:15px;padding-bottom:10px;">Ingrese los valores para la configuraci&oacute;n de <b>dominio</b></div>
 			<table border="0" style="font-size:12px">
 				<tr>
@@ -38,6 +69,52 @@ $env = "dev";
 				<tr>
 					<td>Contrase&ntilde;a:</td>
 					<td><input type="text" name="<? echo $env?>_domainPass" value="<?echo $domainPass?>" style="border:1px solid #444;color:#444;font-style:italic;width:250px;" /></td>
+				</tr>
+			</table>
+			<hr />
+			<div style="font-size:13px;padding-bottom:10px;">Ingrese los valores para la configuraci&oacute;n de <b>base de datos</b></div>
+			<div>
+				<span style="font-size:12px">Driver:</span>
+				<select style="border:1px solid #444;color:#444;font-style:italic;width:250px;">
+					<option value="Mysql">Mysql</option>
+					<option value="new">New</option>
+				</select>
+			</div>
+			<br />
+			<table border="0" style="font-size:12px">
+				<tr>
+					<td colspan="2"><b>Read:</b></td>
+					<td colspan="2" style="padding-left:10px;"><b>Write:</b></td>
+				</tr>
+				<tr>
+					<td>Nombre:</td>
+					<td><input type="text" name="<? echo $env?>_dbRead_name" value="<?echo $dbReadName?>" style="border:1px solid #444;color:#444;font-style:italic;width:100px;" /></td>
+					<td style="padding-left:20px;">Nombre:</td>
+					<td><input type="text" name="<? echo $env?>_dbWrite_name" value="<?echo $dbWriteName?>" style="border:1px solid #444;color:#444;font-style:italic;width:100px;" /></td>
+				</tr>
+				<tr>
+					<td>Host:</td>
+					<td><input type="text" name="<? echo $env?>_dbRead_host" value="<?echo $dbReadHost?>" style="border:1px solid #444;color:#444;font-style:italic;width:100px;" /></td>
+					<td style="padding-left:20px;">Host:</td>
+					<td><input type="text" name="<? echo $env?>_dbWrite_host" value="<?echo $dbWriteHost?>" style="border:1px solid #444;color:#444;font-style:italic;width:100px;" /></td>
+				</tr>
+				<tr>
+					<td>Usario:</td>
+					<td><input type="text" name="<? echo $env?>_dbRead_user" value="<?echo $dbReadUser?>" style="border:1px solid #444;color:#444;font-style:italic;width:100px;" /></td>
+					<td style="padding-left:20px;">Usario:</td>
+					<td><input type="text" name="<? echo $env?>_dbWrite_user" value="<?echo $dbWriteUser?>" style="border:1px solid #444;color:#444;font-style:italic;width:100px;" /></td>
+				</tr>
+				<tr>
+					<td>Contrase&ntilde;a:</td>
+					<td><input type="text" name="<? echo $env?>_dbRead_pass" value="<?echo $dbReadPass?>" style="border:1px solid #444;color:#444;font-style:italic;width:100px;" /></td>
+					<td style="padding-left:20px;">Contrase&ntilde;a:</td>
+					<td><input type="text" name="<? echo $env?>_dbWrite_pass" value="<?echo $dbWritePass?>" style="border:1px solid #444;color:#444;font-style:italic;width:100px;" /></td>
+				</tr>
+				<tr>
+					<td>Puerto:</td>
+					<td><input type="text" name="<? echo $env?>_dbRead_port" value="<?echo $dbReadPort?>" style="border:1px solid #444;color:#444;font-style:italic;width:100px;" /></td>
+					<td style="padding-left:20px;">Puerto:</td>
+					<td><input type="text" name="<? echo $env?>_dbWrite_port" value="<?echo $dbWritePort?>" style="border:1px solid #444;color:#444;font-style:italic;width:100px;" /></td>
 				</tr>
 			</table>
 		</div>
